51511919999afe0445c3468db26cd803958f1a25
Mac Cooper <macooper@csumb.edu>
2018-10-07 02:09:59 +0000

    added the silverjack file to the cst336

---
+++ labs/lab4/index.php
@@ -0,0 +1,111 @@
+<?php
+
+$backgroundImage = "img/sea.jpg";
+
+if (isset($_GET["keyword"])) {  //checks if the form has been submitted
+
+    include "api/pixabayAPI.php";
+
+    $keyword = $_GET["keyword"];
+    echo "You searched for:  $keyword ";
+    
+    
+    echo "Layout: " ." ".  $_GET["layout"];
+
+
+   $imageURLs = getImageURLs($keyword, $_GET["layout"]);
+   //print_r($imageURLs);
+
+   $backgroundImage = $imageURLs[array_rand($imageURLs)];
+  
+}
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+    <head>
+        <title> Lab 4: Pixabay</title>
+        
+        <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" type="text/css" />
+        <link rel="stylesheet" href="css/styles.css" type="text/css" />
+        <style>
+            
+            body {
+                
+                background-image: url(<?=$backgroundImage?>);
+                background-size: cover;
+                
+            }
+            
+            #carouselExampleIndicators{
+                 width:500px;
+                 margin:0 auto; 
+            }
+            
+            h1{
+                color:green;
+            }
+            
+        </style>
+        
+    </head>
+
+
+    <body>
+
+        <form method="GET">
+            
+            <input type="text" name="keyword" size="15" placeholder="Keyword"/>
+            
+            <input type="radio" name="layout" value="horizontal"> Horizontal
+            <input type="radio" name="layout" value="vertical" <?= ($_GET["layout"] ?> Vertical
+            
+            <select name="category">
+                <option value=" "> Select One</option>
+                <option value="bluecar">cars</option>
+                <option value="airplane">plans</option>
+                
+            </select>
+            
+            <input type="submit" name="submitBtn" value="Submit!!" />
+            
+        </form>
+
+        <h1>You must type a keyword or select a category</h1>
+        
+       <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
+          <ol class="carousel-indicators">
+            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
+            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
+          </ol>
+          <div class="carousel-inner">
+            <div class="carousel-item active">
+              <img class="d-block w-100" src="<?=$imageURLs[0]?>" alt="First slide">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="<?=$imageURLs[1]?>" alt="Second slide">
+            </div>
+            <div class="carousel-item">
+              <img class="d-block w-100" src="<?=$imageURLs[2]?>" alt="Third slide">
+            </div>
+          </div>
+          <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
+            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+          </a>
+          <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
+            <span class="carousel-control-next-icon" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+          </a>
+        </div>
+        
+
+
+       <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+       <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js"></script>
+       
+    </body>
+</html>
